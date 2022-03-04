0889a728c2f30b1c096c40159a4b36ca513e9830
“Oner” <onerkerim@me.com>
2022-03-04 15:07:05 +0300

    Django Test uygulamam, ilk commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
